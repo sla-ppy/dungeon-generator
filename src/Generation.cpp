ce3d82f2d8cd93e5b2ca9d50f6ba78a9730bf3e5
sla-ppy <dev@sla-ppy.com>
2022-09-19 00:04:14 +0200

    add room placement checking

--- src/Generation.cpp
+++ src/Generation.cpp
@@ -12,15 +12,38 @@ size_t generate(size_t min, size_t max) {
 }
 
 Error generate(Grid2D& grid) {
-    // TODO: add diff. room sizes with rng
-    // TODO: check if rooms overlap
-    // FIXME: make sure next to room tiles are also in bounds, forgot where we guarantee that
-
-    size_t room_size { 3 };
     for (int i = 0; i < 5; ++i) {
-        // rand room pos
-        const size_t pos_x = Random::generate(0, (grid.width() - room_size));
-        const size_t pos_y = Random::generate(0, (grid.height() - room_size));
+        size_t room_size { Random::generate(2, 4) };
+
+        size_t pos_x;
+        size_t pos_y;
+        bool generating { true };
+        size_t failed_attempts { 0 };
+        while (generating && failed_attempts < 50) {
+            pos_x = Random::generate(1, (grid.width() - room_size - 1)); // rand room pos - check in bounds
+            pos_y = Random::generate(1, (grid.height() - room_size - 1));
+
+            bool failed { false };
+            // check if room fits
+            for (size_t y = pos_y; y < (pos_y + room_size); ++y) {
+                for (size_t x = pos_x; x < (pos_x + room_size); ++x) {
+                    if (failed) {
+                        break;
+                    }
+                    // generate only when empty
+                    if (grid[x][y] != Tile::None) {
+                        failed = true;
+                        failed_attempts++;
+                    }
+                }
+                if (failed) {
+                    break;
+                }
+            }
+            if (!failed) {
+                generating = false;
+            }
+        }
 
         size_t starting_x = pos_x - 1;
         size_t starting_y = pos_y - 1;
