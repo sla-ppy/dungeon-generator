a1b708e743858551d7944c98803aa9eeb6635617
sla-ppy <dev@sla-ppy.com>
2022-09-18 20:54:46 +0200

    change random to multi dib

--- src/Generation.cpp
+++ src/Generation.cpp
@@ -3,18 +3,16 @@
 
 #include <random>
 
-static inline std::random_device rd;
-static inline std::mt19937 gen(rd());
+namespace Random {
+std::mt19937 mt { std::random_device {}() };
 
-int rand_gen(int max) {
-    std::uniform_int_distribution<> distrib(0, max - 1); // here is where you can change the MIN and MAX values of the random dib.
-
-    int rand_result = distrib(gen);
-    return rand_result;
+uint16_t generate(uint16_t min, uint16_t max) {
+    std::uniform_int_distribution set { min, max };
+    return set(mt);
+}
 }
 
 Error generate(Grid2D& grid) {
-    // TODO: when randGen is getting used twice, we need another distribution of values, as we can have multiple and will need multiple
     // TODO: add diff. room sizes with rng
     // TODO: check if rooms overlap
     // FIXME: make sure next to room tiles are also in bounds, forgot where we guarantee that
@@ -22,8 +20,8 @@ Error generate(Grid2D& grid) {
     size_t room_size { 3 };
     for (int i = 0; i < 5; ++i) {
         // rand room pos
-        const size_t pos_x = rand_gen(grid.width() - room_size);
-        const size_t pos_y = rand_gen(grid.height() - room_size);
+        const size_t pos_x = Random::generate(0, (grid.width() - room_size));
+        const size_t pos_y = Random::generate(0, (grid.height() - room_size));
 
         size_t starting_x = pos_x - 1;
         size_t starting_y = pos_y - 1;
@@ -41,13 +39,10 @@ Error generate(Grid2D& grid) {
             }
         }
 
-        bool has_door = false;
-        if (has_door == false) {
-            for (size_t y = 0; y < (pos_y + room_size); ++y) {
-                for (size_t x = 0; x < (pos_x + room_size); ++x) {
-                    if (grid[x][y] == Tile::NextToRoom) {
-                        has_door = true;
-                    }
+        for (size_t y = 0; y < (pos_y + room_size); ++y) {
+            for (size_t x = 0; x < (pos_x + room_size); ++x) {
+                if (grid[x][y] == Tile::NextToRoom) {
+                    grid[x][y] = Tile::Door;
                 }
             }
         }
