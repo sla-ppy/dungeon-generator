b65fdedca011fe7e7187a07f846fd6f62d6c77c6
Lion Kortlepel <development@kortlepel.com>
2022-09-23 14:22:26 +0200

    add STBImage abstraction over stb_image* library

---
+++ src/STBImage.cpp
@@ -0,0 +1,69 @@
+#include "STBImage.h"
+
+#include <algorithm>
+#include <fmt/core.h>
+#include <stb_image_resize.h>
+#include <stb_image_write.h>
+#include <stdexcept>
+
+STBImage::STBImage(std::string_view path, size_t channels)
+    : data { stbi_load(path.data(), &w, &h, &c, channels) } {
+    if (!data) {
+        throw std::runtime_error(fmt::format("stbi_load({}, ...) failed", path));
+    }
+}
+
+STBImage::STBImage(int w, int h, int c)
+    : w(w)
+    , h(h)
+    , c(c)
+    , data(new uint8_t[w * h * c])
+    , manually_allocated(true) {
+    // zero out
+    std::fill_n(data, w * h * c, 0);
+}
+
+STBImage::~STBImage() noexcept {
+    if (data) {
+        if (manually_allocated) {
+            delete[] data;
+        } else {
+            stbi_image_free(reinterpret_cast<void*>(data));
+        }
+    }
+}
+
+STBImage::STBImage(STBImage&& o) noexcept
+    : w(o.w)
+    , h(o.h)
+    , c(o.h)
+    , data(o.data) {
+    // clear other's data to ensure no double-free or
+    // use-after-free bugs
+    o.data = nullptr;
+}
+
+STBImage STBImage::resized(int new_w, int new_h) {
+    STBImage img(new_w, new_h, c);
+    auto ret = stbir_resize_uint8_generic(data, w, h, 0, img.data, new_w, new_h, 0, c,
+        STBIR_ALPHA_CHANNEL_NONE, 0, STBIR_EDGE_CLAMP, STBIR_FILTER_BOX, STBIR_COLORSPACE_LINEAR, nullptr);
+    if (ret == 0) {
+        throw std::runtime_error(fmt::format("resizing image from {}x{} to {}x{} failed", w, h, img.w, img.h));
+    }
+    return img;
+}
+
+uint8_t& STBImage::at(int x, int y, int c_i) {
+    return data[x * h * c + y * c + c_i];
+}
+
+const uint8_t& STBImage::at(int x, int y, int c_i) const {
+    return data[x * h * c + y * c + c_i];
+}
+
+void STBImage::write_to_file_png(std::string_view filename) {
+    const auto ret = stbi_write_png(fmt::format("{}.png", filename).c_str(), w, h, c, data, 0);
+    if (ret == 0) {
+        throw std::runtime_error(fmt::format("failed to write image to '{}.png'", filename));
+    }
+}
---
+++ src/STBImage.h
@@ -0,0 +1,69 @@
+#pragma once
+
+#include <cstddef>
+#include <stb_image.h>
+#include <string_view>
+
+/**
+ * @brief RAII wrapper around the stbi_* api.
+ * This ensures that memory is free'd appropriately.
+ *
+ * stb_image is a C library, so it has no RAII builtin, which
+ * makes using it a massive pain. With this wrapper, the
+ * lifetime of this class results in the
+ */
+struct STBImage {
+    /**
+     * @brief Loads the image into memory, together with size and required number of
+     * channels.
+     * @param path path to image
+     * @param channels number of channels (examples: 3=RGB, 4=RGBA)
+     */
+    STBImage(std::string_view path, size_t channels);
+    /**
+     * @brief Creates an image of `w x h` pixels with `c` channels.
+     * @param w width
+     * @param h height
+     * @param c number of channels (e.g. 3 for RGB)
+     */
+    STBImage(int w, int h, int c);
+    /**
+     * @brief Frees the image memory
+     */
+    ~STBImage() noexcept;
+    // disallow copying, since that would copy the pointer, and after one
+    // instance frees it, its invalid
+    STBImage(const STBImage&) = delete;
+    // ensure moved-from object doesn't hold data
+    STBImage(STBImage&& o) noexcept;
+
+    /**
+     * @brief Returns a copy of the image, resized to the desired `w x h`.
+     * Uses linear colorspace, and a box filter. Assumes *no alpha channel*.
+     * @param new_w desired new width of the image
+     * @param new_h desired new height of the image
+     * @return new image, with its own allocated memory
+     */
+    STBImage resized(int new_w, int new_h);
+
+    uint8_t& at(int x, int y, int c_i);
+    const uint8_t& at(int x, int y, int c_i) const;
+    
+    void write_to_file_png(std::string_view filename);
+
+    // width
+    int w { 0 };
+    // height
+    int h { 0 };
+    // channels
+    int c { 0 };
+    // image data
+    uint8_t* data { nullptr };
+
+private:
+    STBImage() = default;
+
+    // decides whether to use delete[] or stb_image_free
+    // in the destructor (DO NOT USE UNLESS IN resize())
+    bool manually_allocated { false };
+};
